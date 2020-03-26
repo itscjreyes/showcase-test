8336dae9065d4c26cfdcf3f92625b996eac6a463
CJ Reyes <itscjreyes@gmail.com>
2020-03-26 11:44:35 -0400

    Updates

---
+++ precache-manifest.273dc067fbc16c94b1614da008c9c226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5e0c13686055805da0322d9aa2dd3e",
+    "url": "/showcase-test/index.html"
+  },
+  {
+    "revision": "8f807d120edf5cc532fa",
+    "url": "/showcase-test/static/css/main.d998f483.chunk.css"
+  },
+  {
+    "revision": "be8c8aa8b32f2ec242c9",
+    "url": "/showcase-test/static/js/2.fedfb0ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/showcase-test/static/js/2.fedfb0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f807d120edf5cc532fa",
+    "url": "/showcase-test/static/js/main.571d94bc.chunk.js"
+  },
+  {
+    "revision": "fa741a28e7e9a57e08bb",
+    "url": "/showcase-test/static/js/runtime-main.96a3ec92.js"
+  }
+]);
